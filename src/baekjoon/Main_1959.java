caac7fabfaa4fdc4cb672f09807c1013cc819789
baejunghyun36 <bae1004kin@gmail.com>
2022-09-13 18:14:53 +0900

    0913

---
+++ src/baekjoon/Main_1959.java
@@ -0,0 +1,35 @@
+package baekjoon;
+import java.util.*;
+public class Main_1959 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in); 
+		int T; 
+		T = sc.nextInt(); 
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int n, m;  
+			n = sc.nextInt(); 
+			m = sc.nextInt(); 
+			int a[] = new int[n]; 
+			int b[] = new int[m]; 
+			for(int i=0; i<n; i++)a[i] = sc.nextInt(); 
+			for(int i=0; i<m; i++)b[i] = sc.nextInt(); 
+			int sum, max = 0; 
+			for(int k=0; k<n-m+1; k++) {
+				sum=0; 
+				for(int i=0; i<m; i++)sum+=a[i+k]*b[i]; 
+				if(sum>max)max = sum; 
+			}
+			
+			for(int k=0; k<m-n+1; k++) {
+				sum=0; 
+				for(int i=0; i<n; i++)sum+=a[i]*b[i+k]; 
+				if(sum>max)max = sum; 
+			}
+			System.out.format("#%d %d\n", test_case, max); 
+		}
+	}
+
+}
