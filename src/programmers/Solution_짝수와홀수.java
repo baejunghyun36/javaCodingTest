882cffc29e82cd781fb5c365cf5d7d4695ccc0d3
배정현 <bae1004kin@gmail.com>
2022-09-16 02:36:40 +0900

    0916

--- src/programmers/Solution_짝수와홀수.java
+++ src/programmers/Solution_짝수와홀수.java
@@ -2,9 +2,12 @@ package programmers;
 
 public class Solution_짝수와홀수 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    public String solution(int num) {
+        String answer = "";
+       
+        if(num==0||num%2==0) answer = "Even"; 
+        else answer = "Odd"; 
+        return answer;
+    }
 
 }
