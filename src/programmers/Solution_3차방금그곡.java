e2fbc9a63c0d49262986bc5a0623f3724adf0c60
배정현 <bae1004kin@gmail.com>
2022-09-28 23:36:01 +0900

    12

--- src/programmers/Solution_3차방금그곡.java
+++ src/programmers/Solution_3차방금그곡.java
@@ -11,35 +11,73 @@ public class Solution_3차방금그곡 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		num = 0; 
-		String m = "ABCDEFG"; 
+		String m = "CC#BCC#BCC#BCC#B"; 
+		String mm = ""; 
 		musicList = new ArrayList <>();  
-		String	[] musicinfos = {"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"};
-		
+		String	[] musicinfos = {"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"};
+		for(int i=0; i<m.length(); i++) {
+			if(m.charAt(i)=='#') {
+				if(m.charAt(i-1)=='C')	mm+="c"; 
+				else if(m.charAt(i-1)=='D')mm+="d"; 
+				else if(m.charAt(i-1)=='F')mm+="f"; 		
+				else if(m.charAt(i-1)=='G')mm+="g"; 
+				else if(m.charAt(i-1)=='A')mm+="a"; 
+				i++; 
+			}
+			else {
+				
+				mm+=m.charAt(i-1); 
+				if(i==m.length()-1)mm+=m.charAt(i); 
+			}
+		}
+		System.out.println("hihi : "+ mm);
+
 		StringTokenizer st = null;  
-		for(int i=0; i<musicinfos.length; i++) {			
+		for(int i=0; i<musicinfos.length; i++) {	
+			st = new StringTokenizer(musicinfos[i], ","); 
+			String startTime = st.nextToken(); 
+			int sh = Integer.parseInt(startTime.substring(0, 2)); // 시
+			int sm = Integer.parseInt(startTime.substring(3, 5)); // 분
+			sm = sm+ sh*60; 		
+			String endTime = st.nextToken(); 
+			int eh = Integer.parseInt(endTime.substring(0, 2));
+			int em = Integer.parseInt(endTime.substring(3, 5));
+			em = em + eh*60; 			
+			
 			String title = st.nextToken(); 
 			String info = st.nextToken();
+			
 			int cnt = 0; 
-			for(int j=0; j<info.length(); j++) {
-				if(info.charAt(j)=='#')cnt++; 
+			String x = ""; 
+			
+			for(int j=1; j<info.length(); j++) {
+				if(info.charAt(i)=='#') {
+					if(info.charAt(j-1)=='C')	x+="c"; 
+					else if(info.charAt(j-1)=='D')x+="d"; 
+					else if(info.charAt(j-1)=='F')x+="f"; 		
+					else if(info.charAt(j-1)=='G')x+="g"; 
+					else if(info.charAt(j-1)=='A')x+="a"; 
+					j++; 
+				}
+				else {
+					x+=info.charAt(j-1); 
+					if(j==m.length()-1)x+=info.charAt(j); 
+				}
 			}
 			
 			String ss = ""; 
-			int length = info.length(); 
-			for(int j=0; j<(em-sm)/(length-cnt); j++) {
+			int length = x.length(); 
+			for(int j=0; j<(em-sm)/length; j++) {
 				
-				ss+=info;
-				
-			} 
-			for(int j=0; j<(em-sm)%(length-cnt); j++) {
-				ss+=info.charAt(j);
-				if(info.charAt(j+1)=='#') {
-					ss+=info.charAt(j); 
-					j++; 
-				}
+				ss+=x;
 				
 			} 
-			
+			for(int j=0; j<(em-sm)%length; j++) {
+				
+				ss+=x.charAt(j);
+				
+			} 
+			System.out.println(ss);
 			//System.out.println(ss);
 			
 			Music music = new Music (em -sm, num++, title, ss); 
@@ -49,7 +87,7 @@ public class Solution_3차방금그곡 {
 		ArrayList <Music> ans = new ArrayList<>(); 
 		for(Music music : musicList) {
 			String s = music.info; 
-			if(s.contains(m))ans.add(music); 
+			if(s.contains(mm))ans.add(music); 
 		}
 		if(ans.size()==0) {
 			System.out.println("none");
---
+++ src/sw_ability_test/Solution.java
@@ -0,0 +1,42 @@
+package sw_ability_test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+class Solution
+{
+public static void main(String args[]) throws Exception
+{
+
+	Scanner sc = new Scanner(System.in);
+	int T;
+	T=10; 
+	int [] arr = new int[101]; 
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+for(int test_case = 1; test_case <= T; test_case++)
+{
+	int n= Integer.parseInt(in.readLine());
+	StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+	for(int i=0; i<100; i++) {
+		arr[i+1] = Integer.parseInt(st.nextToken()); 
+	}
+
+	for(int i=0; i<n ; i++) {
+		Arrays.sort(arr);
+		if(arr[100]==arr[1])break; 
+		arr[1]++; 
+		arr[100]--; 
+	}
+	
+	Arrays.sort(arr);
+	System.out.printf("#%d %d%n", test_case, arr[100]-arr[1]);
+    }
+  }
+}
+
+
