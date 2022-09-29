91a6f630b5c316b92adb5aea392ff323e4835db8
배정현 <bae1004kin@gmail.com>
2022-09-30 00:42:25 +0900

    0930

--- src/baekjoon/Main_12865.java
+++ src/baekjoon/Main_12865.java
@@ -1,40 +1,64 @@
 package baekjoon;
 
-import java.util.*; 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main_12865 {
 
-	static int N, K; 
-	static int [][] info; 
+	static int N; 
+	static int K; 
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in); 
-		N = sc.nextInt(); //물품의 수
-		K = sc.nextInt(); //버틸 수 있는 무게
-		info = new int [N+1][2]; 
+		N = sc.nextInt(); // itemNumber
+		K = sc.nextInt(); // maxSizeOfBag
 		
-		for(int i=1; i<=N; i++) {
-			
+		ArrayList <Item> itemList = new ArrayList<>(); 
+		itemList.add(new Item(0,0)); // emptyItem; 
+		for(int i=0; i<N; i++) {
 			int w = sc.nextInt(); 
 			int v = sc.nextInt(); 
-			info[i][0] = w; //무게
-			info[i][1] = v; //가치
 			
+			itemList.add(new Item(w,v)); 
 		}
+		
 		
 		int [][] dp = new int [N+1][K+1]; 
+		for(int i=0; i<=N; i++)Arrays.fill(dp[i], 0);
+	
 		
-		for(int i=1; i<=N; i++) {//물품 1부터 N까지
-			for(int j=1; j<=K; j++	 ) {
-				dp[i][j] = dp[i-1][j]; 
-				if(j-info[i][0]>=0) {
-					dp[i][j] = Math.max(dp[i-1][j], info[i][1]+dp[i-1][j-info[i][0]]); 
+		for(int i=1; i<=N; i++) {
+			Item item = itemList.get(i); 
+			for(int j=1; j<=K; j++) {
+				
+				if(item.w<=j) {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-item.w]+item.v); 
 				}
+				else {
+					dp[i][j] = dp[i-1][j]; 
+				}
+				System.out.print(dp[i][j]+" ");
 			}
+			System.out.println();
 		}
+	
+		
 		System.out.println(dp[N][K]);
-
-		StringBuilder sb= new StringBuilder(); 
-		sb.toString(); 
+	}
+	
+	static class Item{
+		int w; //weight
+		int v; //value
+		
+		public Item(int w, int v) {
+			super();
+			this.w = w;
+			this.v = v;
+		} 
+		
+		
 	}
 
 }
