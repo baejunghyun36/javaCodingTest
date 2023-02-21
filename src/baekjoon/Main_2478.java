3fdcfeb792a437bee7384568a620a014c268f327
baejunghyun36 <bae1004kin@gmail.com>
2023-02-21 19:37:51 +0900

    0221

---
+++ src/baekjoon/Main_2478.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main_2478 {
+
+  static int N;
+  static int[] info;
+  static List<Integer> list;
+  public static void main(String[] args) throws IOException {
+
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+    //StringTokenizer st = new StringTokenizer();
+
+
+
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
