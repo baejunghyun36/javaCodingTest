7927dd8e9e9d4fe446a05b2d05b0a8b858069d74
baejunghyun36 <bae1004kin@gmail.com>
2023-03-29 00:16:21 +0900

    0329

---
+++ src/programmers/풍선터트리기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class 풍선터뜨리기 {
+  static int [] dp1, dp2;
+  public int solution(int[] a) {
+
+    int answer = 0;
+
+    dp1 = new int [a.length];
+    dp2 = new int [a.length];
+
+    dp1[0] = a[0];
+    dp2[a.length-1] = a[a.length-1];
+
+    for(int i=1; i<a.length; i++) dp1[i] = Math.min(dp1[i-1], a[i]);
+    for(int i=a.length-2; i>=0; i--) dp2[i] = Math.min(dp2[i+1], a[i]);
+
+    if(a.length<=2)answer = a.length;
+    else{
+      answer = 2;
+      for(int i= 1; i<a.length-1; i++){
+        if(!(a[i]>dp1[i-1]&&a[i]>dp2[i+1]))  answer++;
+      }
+    }
+
+    return answer;
+  }
+}
