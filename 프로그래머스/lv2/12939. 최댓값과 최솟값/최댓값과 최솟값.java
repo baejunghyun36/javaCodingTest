501b1a3a83150e1977cb6cffd7275f5dae773120
bae3007 <78024741+baejunghyun36@users.noreply.github.com>
2023-04-09 00:21:44 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 14.67 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        String[] ss = s.split(" "); 
+        int [] arr = new int[ss.length]; 
+        for(int i=0; i<ss.length; i++){
+            arr[i] = Integer.parseInt(ss[i]); 
+        }
+        Arrays.sort(arr); 
+        answer+=arr[0]+" "+arr[arr.length-1]; 
+        
+        return answer;
+    }
+}
