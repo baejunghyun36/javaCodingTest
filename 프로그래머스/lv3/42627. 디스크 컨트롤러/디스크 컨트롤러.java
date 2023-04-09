75909c0ad38cb71beed2f168cc118552281424e6
bae3007 <78024741+baejunghyun36@users.noreply.github.com>
2023-04-09 14:25:37 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.79 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,59 @@
+import java.util.*; 
+class Solution {
+    static PriorityQueue <Job> pq; 
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        Arrays.sort(jobs, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            } else {
+                return o1[0] - o2[0];
+            }
+         });
+        pq = new PriorityQueue<>(); 
+        
+        int finishTime = jobs[0][0] + jobs[0][1]; 
+        int time = finishTime - jobs[0][0]; 
+        int index = 1; 
+        while(true){
+            while(index<jobs.length){
+                if(finishTime>=jobs[index][0]){
+                    pq.add(new Job(jobs[index][0], jobs[index][1])); 
+                    index++; 
+                }
+                else break; 
+            }
+            
+            if(pq.isEmpty()){
+               if(index==jobs.length)break; 
+               finishTime = jobs[index][0] + jobs[index][1];
+               time += finishTime - jobs[index][0]; 
+               index++; 
+            }
+            else{
+                Job job = pq.poll(); 
+                finishTime += job.workingTime; 
+                time+= finishTime - job.startTime; 
+            }
+        }
+        return time/jobs.length;
+    }
+    
+    static class Job implements Comparable<Job> {
+        
+        int startTime; 
+        int workingTime; 
+        
+        public Job (int st, int wt){
+            this.startTime = st; 
+            this.workingTime = wt; 
+        }
+        
+        public int compareTo(Job job){
+            return this.workingTime - job.workingTime; 
+        }
+        
+    }
+    
+
+}
