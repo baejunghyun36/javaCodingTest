c7add8299946389a7e134f60455780c2a10726a5
bae3007 <bae1004kin@gmail.com>
2023-07-03 19:54:09 +0900

    [Gold III] Title: 네잎 클로버를 찾아서, Time: 968 ms, Memory: 64752 KB -BaekjoonHub

--- 백준/Gold/3089. 네잎 클로버를 찾아서/네잎 클로버를 찾아서.java
+++ 백준/Gold/3089. 네잎 클로버를 찾아서/네잎 클로버를 찾아서.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Main {
     static int n, m;
     static List<Clova> clovaList;
-    static ArrayList<int[]> xList[];
-    static ArrayList<int[]> yList[];
+    static ArrayList<Integer> xList[];
+    static ArrayList<Integer> yList[];
     static Clova cur;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,14 +28,13 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken())+100000;
             int y = Integer.parseInt(st.nextToken())+100000;
-            xList[x].add(new int[]{y, i});
-            yList[y].add(new int[]{x, i});
-            clovaList.add(new Clova(y, x));
+            xList[x].add(y);
+            yList[y].add(x);
         }
 
         for(int i=0; i<200002; i++){
-            Collections.sort(xList[i], (o1, o2)->o1[0]-o2[0]);
-            Collections.sort(yList[i], (o1, o2)->o1[0]-o2[0]);
+            Collections.sort(xList[i]);
+            Collections.sort(yList[i]);
         }
 
         String dir = br.readLine();
@@ -43,12 +42,10 @@ public class Main {
 
         for(int i=0; i<dir.length(); i++){
             char d = dir.charAt(i);
-            int index = 0;
-            if(d=='U') cur.y = xList[cur.x].get(binarySearch(cur.y, xList[cur.x])+1)[0];
-            else if(d=='D') cur.y = xList[cur.x].get(binarySearch(cur.y, xList[cur.x])-1)[0];
-            else if(d=='L') cur.x = yList[cur.y].get(binarySearch(cur.x, yList[cur.y])-1)[0];            
-            else cur.x = yList[cur.y].get(binarySearch(cur.x, yList[cur.y])+1)[0];
-            
+            if(d=='U') cur.y = xList[cur.x].get(binarySearch(cur.y, xList[cur.x])+1);
+            else if(d=='D') cur.y = xList[cur.x].get(binarySearch(cur.y, xList[cur.x])-1);
+            else if(d=='L') cur.x = yList[cur.y].get(binarySearch(cur.x, yList[cur.y])-1);
+            else cur.x = yList[cur.y].get(binarySearch(cur.x, yList[cur.y])+1);
         }
         sb.append((cur.x-100000)+" "+(cur.y-100000));
         bw.write(sb.toString());
@@ -57,14 +54,14 @@ public class Main {
         br.close();
     }
 
-    static int binarySearch (int value, ArrayList<int[]> list){
+    static int binarySearch (int value, ArrayList<Integer> list){
 
         int l = 0;
         int h = list.size()-1;
         while(l<=h){
             int mid = (l+h)/2;
-            if(list.get(mid)[0]==value) return mid;            
-            else if(list.get(mid)[0]>value)  h = mid-1;            
+            if(list.get(mid)==value) return mid;
+            else if(list.get(mid)>value)  h = mid-1;
             else l = mid+1;
         }
         return 1;
