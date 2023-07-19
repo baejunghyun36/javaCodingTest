c0d945ada7bd9f28ab17caa67a35eae8e770625e
bae3007 <bae1004kin@gmail.com>
2023-07-19 21:31:08 +0900

    [Gold V] Title: Australian Voting, Time: 144 ms, Memory: 13864 KB -BaekjoonHub

--- 백준/Gold/4419. Australian Voting/Australian Voting.java
+++ 백준/Gold/4419. Australian Voting/Australian Voting.java
@@ -11,15 +11,12 @@ public class Main {
     static boolean [] removeList;
     static boolean [] removeUserList;
     static int m;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
-        userSelections = new LinkedList[2001];
+        userSelections = new LinkedList[1001];
         candidateList = new Candidate[n + 1];
         candidateNumberToName = new HashMap<>();
         removeList = new boolean[n+1];
@@ -29,25 +26,16 @@ public class Main {
             candidateList[i] = new Candidate(i, 0);
             candidateNumberToName.put(i, name);
         }
-
-        for(int i=1; i<=1500; i++)  userSelections[i] = new LinkedList<>();
+        for(int i=1; i<=1000; i++)  userSelections[i] = new LinkedList<>();
         String line;
-        int index = 1;
+
         while((line = br.readLine()) != null && line.length() > 0) {
             st = new StringTokenizer(line); // 각 줄에는 투표 내역이 입력된다.
-            for(int i=0; i<n; i++){
-                userSelections[index].add(Integer.parseInt(st.nextToken()));
-            }
-            index++;
+            for(int i=0; i<n; i++) userSelections[m+1].add(Integer.parseInt(st.nextToken()));
             m++;
         }
         removeUserList = new boolean[n + 1];
-        sb.append(calculateVotingPercentage());
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(calculateVotingPercentage());
     }
 
     static String calculateVotingPercentage(){
@@ -55,8 +43,8 @@ public class Main {
         while(true){
             percentageList = new double[n + 1];
             for(int i=1; i<=m; i++){
-                while(userSelections[i].size()>0&&removeList[userSelections[i].get(0)]==true) userSelections[i].remove(0);
-                if(userSelections[i].size()!=0) percentageList[userSelections[i].get(0)]++;
+                while(removeList[userSelections[i].get(0)]==true) userSelections[i].remove(0);
+                percentageList[userSelections[i].get(0)]++;
             }
             ArrayList<Candidate> temp = new ArrayList<>();
             for(int i=1; i<=n; i++){
@@ -66,10 +54,6 @@ public class Main {
                 temp.add(new Candidate(i, percentageList[i]));
             }
             Collections.sort(temp);
-            if(temp.size()>0&&temp.get(0).percent>=50){
-                if(temp.size()==2&&temp.get(1).percent==50) return candidateNumberToName.get(temp.get(0).number) + " " + candidateNumberToName.get(temp.get(1).number);
-                return candidateNumberToName.get(temp.get(0).number);
-            }
             double minPercent = temp.get(temp.size()-1).percent;
             int index = temp.size()-1;
             if(temp.get(0).percent == temp.get(index).percent){
@@ -81,6 +65,7 @@ public class Main {
                 }
                 return s;
             }
+            else if(temp.get(0).percent>=50) return candidateNumberToName.get(temp.get(0).number);
             while (index>=0&&minPercent == temp.get(index).percent) {
                 removeList[temp.get(index).number] = true;
                 index--;
@@ -103,26 +88,3 @@ public class Main {
         }
     }
 }
-
-
-/*
-*
-
-5
-a
-b
-c
-d
-e
-1 2 3 4 5
-1 2 3 4 5
-2 3 1 5 4
-2 3 1 5 4
-3 4 5 1 2
-3 4 5 1 2
-5 1 2 3 4
-4 3 1 2 5
-
-*
-*
-* */
