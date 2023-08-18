38ddb1bbf98ee798a6007b8f6de42086afc9c1d5
bae3007 <bae1004kin@gmail.com>
2023-08-19 00:58:47 +0900

    블록 이동하기.java

--- 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.java
+++ 프로그래머스/lv3/60063. 블록 이동하기/블록 이동하기.java
@@ -24,30 +24,25 @@ class Solution {
         while(!q.isEmpty()){
             Robot r = q.poll(); 
             int dist = visited[r.isVertical][r.y][r.x]; 
-            int rrx = 0; 
-            int rry = 0; 
             if(r.isVertical==1){
-                rrx = r.x; 
-                rry = r.y+1; 
-                if(rry==n-1&&rrx==n-1)return dist; 
+                if(r.y+1==n-1&&r.x==n-1)return dist; 
             }
             else{
-                rrx = r.x+1; 
-                rry = r.y; 
-                if(rry==n-1&&rrx==n-1)return dist; 
+                if(r.y==n-1&&r.x+1==n-1)return dist; 
             }
             for(int i=0; i<4; i++){
                 int lx = r.x + dx[i]; 
                 int ly = r.y + dy[i]; 
-                int rx = rrx + dx[i]; 
-                int ry = rry + dy[i]; 
-                
+                int rx = r.x + dx[i]; 
+                int ry = r.y + dy[i]; 
+                if(r.isVertical==1)ry++; 
+                else rx++; 
                 if(lx<0||lx>=n||rx<0||rx>=n||ly<0||ly>=n||ry<0||ry>=n)continue; 
-                if(map[ly][lx]==1||map[ry][rx]==1)continue; 
+                if(map[ly][lx]==1||map[ry][rx]==1)continue;                 
                 if(visited[r.isVertical][ly][lx]==0){
                     visited[r.isVertical][ly][lx] = dist + 1; 
                     q.add(new Robot(ly, lx, r.isVertical));
-                }
+                } 
                 int isV = r.isVertical^1; 
                 if(i==0&&r.isVertical==0){
                     //아래일 때 
@@ -66,23 +61,18 @@ class Solution {
                 }
                 else if(i==3&&r.isVertical==1){
                     //오른쪽
-                    // System.out.println("hi"); 
                     rangeCheck(isV, r.y, r.x, dist+1); 
                     rangeCheck(isV, r.y+1, r.x, dist+1); 
                 }
             }
-            // for(int i=0; i<n; i++)System.out.println(Arrays.toString(visited[1][i])); 
-            // System.out.println(); 
         }
         return -1; 
-
     }
     static void rangeCheck(int isVertical, int y, int x, int dist){
 
         if(visited[isVertical][y][x]==0){
             visited[isVertical][y][x] = dist; 
             q.add(new Robot(y, x, isVertical)); 
-             // System.out.println(ly+ " " + lx + " "+ ry+ " "+ rx); 
         }
     }
     
