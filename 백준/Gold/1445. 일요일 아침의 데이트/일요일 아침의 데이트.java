c4b3713e9acfc09e4a68e9ef35ca4aa3b5b5a5ae
bae3007 <bae1004kin@gmail.com>
2023-08-20 18:54:45 +0900

    [Gold II] Title: 일요일 아침의 데이트, Time: 92 ms, Memory: 11900 KB -BaekjoonHub

--- 백준/Gold/1445. 일요일 아침의 데이트/일요일 아침의 데이트.java
+++ 백준/Gold/1445. 일요일 아침의 데이트/일요일 아침의 데이트.java
@@ -5,13 +5,13 @@ public class Main {
 
     static char [][] map;
     static int m, n;
-    static Garbage [][] garbageMap;
     static char [][] aroundMap;
     static int [] start, end;
-    static Queue<int []> q;
+    static int [][] visited;
+    static PriorityQueue<Node> q;
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {-1, 1, 0, 0};
-    static final int INF = Integer.MAX_VALUE;
+    static final int INF = 987654321;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -20,35 +20,26 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
         start = new int[2];
-
-        q = new LinkedList<>();
+        visited = new int [m][n];
+        q = new PriorityQueue<>();
         end = new int[2];
         map = new char[m][n];
         aroundMap = new char[m][n];
-        garbageMap = new Garbage[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                garbageMap[i][j] = new Garbage(INF, INF);
-            }
-        }
+
+
         for (int i = 0; i < m; i++) {
             map[i] = br.readLine().toCharArray();
             for (int j = 0; j < n; j++) {
+
                 if(map[i][j]=='S'){
                     start[0] = i;
                     start[1] = j;
-                    garbageMap[i][j].cnt = 0;
-                    garbageMap[i][j].around = 0;
                 }
                 else if(map[i][j]=='F'){
                     end[0] = i;
                     end[1] = j;
                 }
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(map[i][j] == 'g'){
+                else if(map[i][j] == 'g'){
                     for (int d = 0; d < 4; d++) {
                         int nx = dx[d] + j;
                         int ny = dy[d] + i;
@@ -60,71 +51,51 @@ public class Main {
         }
         aroundMap[start[0]][start[1]] = '.';
         aroundMap[end[0]][end[1]] = '.';
-        q.add(new int[]{start[0], start[1]});
-        bfs();
-        Garbage endPoint = garbageMap[end[0]][end[1]];
-        sb.append(endPoint.cnt + " " +endPoint.around);
+        visited[start[0]][start[1]] = 1;
+        q.add(new Node(0, 0, start[0], start[1]));
+        Node endNode = dijk();
+        sb.append(endNode.garbage + " " +endNode.around);
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
-    static void bfs(){
-        Garbage cmp = new Garbage(0,0);
-        while (!q.isEmpty()) {
-            int[] yx = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int ny = yx[0] + dy[i];
-                int nx = yx[1] + dx[i];
-                if(ny<0||ny>=m||nx<0||nx>=n)continue;
-                Garbage cur = garbageMap[yx[0]][yx[1]];
-                cmp.cnt = cur.cnt;
-                cmp.around = cur.around;
-//                System.out.println(map[ny][nx]);
-                if(map[ny][nx]=='g') cmp.cnt++;
-                else if(aroundMap[ny][nx]=='a') cmp.around++;
-                Garbage next = garbageMap[ny][nx];
-                boolean flag = false;
 
-                if(next.cnt>cmp.cnt){
-                    next.cnt = cmp.cnt;
-                    next.around = cmp.around;
-                    flag = true;
-                }
-                else if (next.cnt == cmp.cnt && next.around > cmp.around) {
-                    next.around = cmp.around;
-                    flag = true;
-                }
-                if(flag){
-                     q.add(new int[]{ny, nx});
-                }
+    static Node dijk(){
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+            if(cur.y==end[0]&&cur.x == end[1]) return cur;
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i] + cur.x;
+                int ny = dy[i] + cur.y;
+                if(ny<0||ny>=m||nx<0||nx>=n||visited[ny][nx]==1)continue;
+                Node next = new Node(cur.garbage, cur.around, ny, nx);
+                if(map[ny][nx]=='g') next.garbage++;
+                else if(aroundMap[ny][nx]=='a')next.around++;
+                q.add(next);
+                visited[ny][nx] = 1;
             }
         }
+        return new Node(0,0,0,0);
     }
-    static class Garbage {
 
-        int cnt;
+    static class Node implements Comparable<Node> {
+
+        int garbage;
         int around;
+        int y;
+        int x;
 
-        public Garbage( int cnt, int around) {
-            this.cnt = cnt;
+        public Node( int garbage, int around, int y, int x) {
+            this.garbage = garbage;
             this.around = around;
+            this.y = y;
+            this.x = x;
+        }
+
+        public int compareTo(Node node) {
+            if(this.garbage == node.garbage) return this.around - node.around;
+            return this.garbage - node.garbage;
         }
     }
 }
-
-
-/*
-3 3
-ggF
-ggg
-Sgg
-
-                   for (int k = 0; k < n; k++) {
-                        for (int j = 0; j < m; j++) {
-                            System.out.print(garbageMap[k][j].cnt+" ");
-                        }
-                        System.out.println();
-                    }
-                    System.out.println();
-*/
