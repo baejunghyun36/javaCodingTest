bb98895482f6036b73326b5ef69d289948b42cae
bae3007 <bae1004kin@gmail.com>
2023-09-02 13:55:43 +0900

    [unrated] Title: 우박수열 정적분, Time: 28.61 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134239. 우박수열 정적분/우박수열 정적분.java
@@ -0,0 +1,53 @@
+import java.util.*; 
+class Solution {
+    static Queue <int []> q; 
+    static ArrayList<Integer> yList; 
+    public double[] solution(int k, int[][] ranges) {
+        
+        ArrayList<Double> answerList = new ArrayList<>(); 
+        int cnt = 0; 
+        yList = new ArrayList<>(); 
+        while(k!=1){
+ 
+            yList.add(k); 
+            if(k%2==0) k/=2; 
+            else k = k*3+1;      
+            
+            cnt++; 
+        }
+        yList.add(1); 
+        for(int i=0; i<ranges.length; i++){
+            int start = ranges[i][0]; 
+            int end = ranges[i][1]; 
+            if(end<=0) end = yList.size()-1+ranges[i][1]; 
+     
+            if(start>end) answerList.add(-1.0); 
+            else if(start==end) answerList.add(0.0); 
+            else{
+                int index = 0; 
+                int prevY = yList.get(start); 
+                int prevX = start; 
+                double area = 0; 
+                for(int x = start+1; x<=end; x++){
+                    int y = yList.get(x); 
+                    area += Math.min(y, prevY); 
+                    area += (Math.max(y, prevY) - Math.min(y, prevY))/2.0; 
+                    prevY = y; 
+                }
+                answerList.add(area); 
+            }
+ 
+            System.out.println(); 
+        }
+        // System.out.println(cnt); 
+        double []answer = new double[answerList.size()]; 
+        for(int i=0; i<answerList.size(); i++){
+            answer[i] = answerList.get(i); 
+        }
+        
+        
+        return answer;
+    }
+    
+
+}
