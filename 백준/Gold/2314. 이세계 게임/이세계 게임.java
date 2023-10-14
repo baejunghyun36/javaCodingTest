d65aa038620b93246b6aff800b2cacd8a4c6a6d2
bae3007 <bae1004kin@gmail.com>
2023-10-14 14:05:41 +0900

    [Gold III] Title: 이세계 게임, Time: 80 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Gold/2314. 이세계 게임/이세계 게임.java
@@ -0,0 +1,117 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int [][] map;
+    static int [][] goal;
+    static int[] dx = {1, -1, 0};
+    static int[] dy = {0, 0, 1};
+    static int n = 4;
+    static int [][] visited;
+    static int [][] fixed;
+    static Queue<int[]> q;
+    static int answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        map = new int[n][n];
+        goal = new int[n][n];
+        for (int i = 0; i < 4; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < 4; j++) {
+                if(s.charAt(j)=='P')map[i][j] = 1;
+            }
+        }
+        br.readLine();
+        for (int i = 0; i < 4; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < 4; j++) {
+                if(s.charAt(j)=='P')goal[i][j] = 1;
+            }
+        }
+
+        fixed = new int[n][n];
+
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if(map[i][j]==goal[i][j]){
+                    fixed[i][j] = 1;
+                }
+                else{
+
+                    q = new LinkedList<>();
+                    visited = new int[n][n];
+                    visited[i][j] = 1;
+                    q.add(new int[]{i, j, 0});
+                    bfs(map[i][j]);
+                    map[i][j] = goal[i][j];
+                    fixed[i][j] = 1;
+//                    System.out.println("변경된 map");
+//                    for (int k = 0; k < 4; k++) {
+//                        System.out.println(Arrays.toString(map[k]));
+//                    }
+//                    System.out.println(answer+" an");
+//                    System.out.println();
+                }
+            }
+        }
+
+        sb.append(answer);
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void bfs(int type){
+
+        while (!q.isEmpty()) {
+
+
+            int [] yx = q.poll();
+            for (int i = 0; i < 3; i++) {
+                int nx = yx[1] + dx[i];
+                int ny = yx[0] + dy[i];
+                if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+                if (visited[ny][nx] == 1 || fixed[ny][nx] == 1) continue;
+                if (map[ny][nx] != type && goal[ny][nx]!=map[ny][nx]) {
+
+                    map[ny][nx] = type;
+                    answer += yx[2] + 1;
+//                    System.out.println("방문 처리");
+//                    for (int k = 0; k < n; k++) {
+//                        System.out.println(Arrays.toString(visited[k]));
+//                    }
+//                    System.out.println();
+
+                    return;
+                }
+                visited[ny][nx] = 1;
+                q.add(new int[]{ny, nx, yx[2] + 1});
+            }
+
+
+
+        }
+    }
+
+}
+
+/*
+LLLP
+PPPP
+LPLP
+PLLL
+
+LPPL
+LPLL
+PPPP
+LLLP
+
+정답 7
+* */
