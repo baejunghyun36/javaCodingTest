fdc8d95b791faae3640296645d4a53fd805834ea
bae3007 <bae1004kin@gmail.com>
2023-10-18 14:13:15 +0900

    [Gold III] Title: 문자판, Time: 352 ms, Memory: 17308 KB -BaekjoonHub

--- 백준/Gold/2186. 문자판/문자판.java
+++ 백준/Gold/2186. 문자판/문자판.java
@@ -9,7 +9,9 @@ public class Main {
     static char [] str;
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
@@ -20,20 +22,15 @@ public class Main {
         k = Integer.parseInt(st.nextToken());
 
         map = new char[m][n];
-        for (int i = 0; i < m; i++) {
-            map[i] = br.readLine().toCharArray();
-        }
-
+        for (int i = 0; i < m; i++) map[i] = br.readLine().toCharArray();
         str = br.readLine().toCharArray();
-
         dp = new int[str.length][m][n];
         for (int i = 0; i < str.length; i++) {
             for (int j = 0; j < m; j++) {
-                for(int k=0; k<n; k++){
-                   dp[i][j][k] = -1;
-                }
+                Arrays.fill(dp[i][j], -1);
             }
         }
+
         int answer = 0;
         for (int i = 0; i < m; i++) {
             char c = str[0];
@@ -41,6 +38,7 @@ public class Main {
                 if(map[i][j]==c)answer += dfs(i, j, 1);
             }
         }
+
         sb.append(answer);
         bw.write(sb.toString());
         bw.flush();
@@ -50,26 +48,19 @@ public class Main {
 
     static int dfs(int i, int j, int index) {
 
-        if(index==str.length){
-            return 1;
-        }
+        if(index==str.length) return 1;
         if(dp[index][i][j]!=-1) return dp[index][i][j];
         dp[index][i][j] = 0;
-
-
         for (int d = 0; d < 4; d++) {
             int nx = j;
             int ny = i;
             for (int o = 0; o < k; o++) {
                 nx += dx[d];
                 ny += dy[d];
-                if(ny<0||ny>=m||nx<0||nx>=n)continue;
-                if(map[ny][nx] == str[index])
-                    dp[index][i][j] += dfs(ny, nx, index + 1);
+                if(ny<0||ny>=m||nx<0||nx>=n) break;
+                if(map[ny][nx] == str[index]) dp[index][i][j] += dfs(ny, nx, index + 1);
             }
         }
         return dp[index][i][j];
-
-
     }
 }
