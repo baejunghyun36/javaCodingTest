bcfd3fd52293a1117f9e14a8f9768f6fca75df10
bae3007 <bae1004kin@gmail.com>
2023-10-18 17:39:03 +0900

    [Gold III] Title: 코딩은 예쁘게, Time: 88 ms, Memory: 11948 KB -BaekjoonHub

--- 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.java
+++ 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.java
@@ -1,13 +1,11 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-
     static int n;
     static int [] arr1, arr2;
     static int [] updatedArr;
+    static int answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -18,51 +16,32 @@ public class Main {
         arr2 = new int[n+1];
         updatedArr = new int[n + 1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            arr1[i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 1; i <= n; i++) arr1[i] = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            arr2[i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 1; i <= n; i++) arr2[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) updatedArr[i] = arr1[i] - arr2[i];
 
-        for (int i = 1; i <= n; i++) {
-            updatedArr[i] = arr1[i] - arr2[i];
-        }
-
-        int answer = 0;
-        for (int i = 1; i <= 80; i++) {
-            boolean flag = false;
-            for (int j = 1; j <= n; j++) {
-                if(updatedArr[j]<=0) {
-                    if(flag)answer++;
-                    flag = false;
-                    continue;
-                }
-                updatedArr[j]--;
-                flag = true;
-            }
-            if(flag) answer++;
-        }
-        for (int i = 1; i <= 80; i++) {
-            boolean flag = false;
-            for (int j = 1; j <= n; j++) {
-                if(updatedArr[j]>=0) {
-                    if(flag)answer++;
-                    flag = false;
-                    continue; 
-                }
-                updatedArr[j]++;
-                flag = true;
-            }
-            if(flag) answer++;
-        }
-
-        sb.append(answer);
-
+        if(n==1) sb.append(updatedArr[1]);
+        else sb.append(solution());
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    static int solution(){
+
+        int prev = updatedArr[1];
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (prev * updatedArr[i] < 0) {
+                answer += Math.abs(prev);
+            }
+            else if (Math.abs(prev) > Math.abs(updatedArr[i])) {
+                answer += Math.abs(prev) - Math.abs(updatedArr[i]);
+            }
+            prev = updatedArr[i];
+        }
+        return answer += Math.abs(prev);
     }
 }
