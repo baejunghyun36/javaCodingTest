ceb0b9897728e53e2f2c658cb7aaa219b8ce7b7f
bae3007 <bae1004kin@gmail.com>
2023-10-31 10:18:07 +0900

    [Gold II] Title: 트리, Time: 372 ms, Memory: 38912 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.java
+++ 백준/Gold/4256. 트리/트리.java
@@ -5,61 +5,50 @@ public class Main {
 
     static int testCase;
     static int n;
-    static int [] preorder, inorder, postorder;
+    static int [] preorder;
     static Map <Integer, Integer> indexOfNumber;
     static int postIndex;
     static int preIndex;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         testCase = Integer.parseInt(st.nextToken());
+
         while (testCase-- > 0) {
-
             n = Integer.parseInt(br.readLine());
-
             preorder = new int[n];
-            inorder = new int[n];
-            postorder = new int[n];
             indexOfNumber = new HashMap<>();
             preIndex = postIndex = 0;
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                preorder[i] = Integer.parseInt(st.nextToken());
-            }
+            for (int i = 0; i < n; i++) preorder[i] = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                inorder[i] = Integer.parseInt(st.nextToken());
-                indexOfNumber.put(inorder[i], i);
-            }
-            makePostorder(preorder[0], 0, n-1);
-            for (int i = 0; i < n; i++) {
-                sb.append(postorder[i]).append(" ");
-            }
+            for (int i = 0; i < n; i++) indexOfNumber.put(Integer.parseInt(st.nextToken()), i);
+            makePostorder(0, n-1);
             sb.append("\n");
         }
-
-
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
-    static void makePostorder(int curNumber, int start, int end) {
+    static void makePostorder(int start, int end) {
 
-        if(start>end) {
-            preIndex--;
-            return;
+        if (preIndex >= n) return;
+
+        int cur = preorder[preIndex];
+        int mid = indexOfNumber.get(cur);
+        if (start < mid) {
+            preIndex++;
+            makePostorder(start, mid-1);
         }
-        if (start == end) {
-            postorder[postIndex++] = inorder[start];
-            return;
+        if (mid < end) {
+            preIndex++;
+            makePostorder(mid + 1, end);
         }
-        int mid = indexOfNumber.get(curNumber);
-        if(preIndex<n-1)makePostorder(preorder[++preIndex], start, mid-1);
-        if(preIndex<n-1)makePostorder(preorder[++preIndex], mid + 1, end);
-        postorder[postIndex++] = curNumber;
+        sb.append(cur).append(" ");
     }
 }
