58b0ae0c3307457cee05ec1de03941439753738a
bae3007 <bae1004kin@gmail.com>
2023-11-17 13:18:04 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,6 @@
-SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME
-FROM ANIMAL_INS A
-JOIN ANIMAL_OUTS B
-ON A.ANIMAL_ID = B.ANIMAL_ID
-WHERE A.SEX_UPON_INTAKE != B.SEX_UPON_OUTCOME
-ORDER BY A.ANIMAL_ID
+select a.animal_id, a.animal_type, a.name
+from animal_ins a
+join animal_outs b
+on a.animal_id = b.animal_id
+where a.sex_upon_intake != b.sex_upon_outcome
+order by a.animal_id
