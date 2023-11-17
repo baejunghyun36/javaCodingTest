22adace48837160f3fea59253b3c99f70e678125
bae3007 <bae1004kin@gmail.com>
2023-11-17 13:04:22 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,7 @@
-select a.name, a.datetime
-from animal_ins a
-left join animal_outs b
-on a.animal_id = b.animal_id
-where b.datetime is null
+SELECT a.name, a.datetime
+FROM animal_ins a
+LEFT JOIN animal_outs b ON a.animal_id = b.animal_id
+WHERE b.SEX_UPON_OUTCOME IS NULL
 order by a.datetime
 limit 3
+
