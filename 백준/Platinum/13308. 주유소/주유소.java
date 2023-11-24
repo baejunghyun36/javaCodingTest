c868aefb14455369dcd51d93bdf238861d314a71
bae3007 <bae1004kin@gmail.com>
2023-11-24 18:15:31 +0900

    [Platinum V] Title: 주유소, Time: 352 ms, Memory: 44544 KB, Score: 34 point -BaekjoonHub

---
+++ 백준/Platinum/13308. 주유소/주유소.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.*;
+
+public class Main {
+
+
+    static PriorityQueue<Node> pq;
+    static int n, m;
+    static List<Node> [] list;
+    static int [] cost;
+    static int [] payOil;
+    static final int INF = 987654321;
+    static int [][] dp;
+    static int minVertex, minPayOil, costToMinVertex;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        pq = new PriorityQueue<>();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        payOil = new int[n + 1];
+        minPayOil = INF;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            payOil[i + 1] = Integer.parseInt(st.nextToken());
+            if(i!=n-1)minPayOil = Math.min(minPayOil, payOil[i + 1]);
+        }
+
+        list = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) list[i] = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list[a].add(new Node(b, 0, c, INF));
+            list[b].add(new Node(a, 0, c, INF));
+        }
+        dp = new int[2501][n + 1];
+        for (int i = 0; i <= 2500; i++) Arrays.fill(dp[i], INF);
+        dp[0][1] = 0;
+        pq.add(new Node(1, 0, 0, payOil[1]));
+        sb.append(dijk());
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+    static int dijk(){
+
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+
+            if (dp[cur.payPerl][cur.vertex] < cur.cost) {
+                continue;
+            }
+            for (int i = 0; i < list[cur.vertex].size(); i++) {
+                Node next = list[cur.vertex].get(i);
+                int nextCost = cur.cost + cur.payPerl * next.dist;
+
+                if (dp[cur.payPerl][next.vertex] > nextCost) {
+                    dp[cur.payPerl][next.vertex] = nextCost;
+                    pq.add(new Node(next.vertex, nextCost, 0, Math.min(cur.payPerl, payOil[next.vertex])));
+                }
+            }
+        }
+        int cost = INF;
+        for(int i=0; i<=2500; i++){
+            cost = Math.min(dp[i][n], cost);
+        }
+
+        return cost;
+    }
+    static class Node implements Comparable<Node> {
+
+        int vertex;
+        int cost;
+        int dist;
+        int payPerl;
+
+        public Node(int vertex, int cost, int dist, int payPerl) {
+            this.vertex = vertex;
+            this.cost = cost;
+            this.dist = dist;
+            this.payPerl = payPerl;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - payPerl;
+        }
+    }
+}
